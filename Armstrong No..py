195766c004a2950d314e8351a94b1056e0aff20e
Dhaarmi Mewada <mewadadhaarmi@gmail.com>
2026-03-16 19:14:33 +0530

    Add files via upload

---
+++ Armstrong No..py
@@ -0,0 +1,16 @@
+# Check Armstrong number
+
+num = int(input("Enter a number: "))
+
+original = num
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + digit**3
+    num = num // 10
+
+if sum == original:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
