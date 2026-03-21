de5ee583ab5b5b789479e786f70830ad6ae73c53
Dhaarmi Mewada <mewadadhaarmi@gmail.com>
2026-03-21 19:17:05 +0530

    Add files via upload

---
+++ reverse an array.py
@@ -0,0 +1,13 @@
+# Reverse an array
+
+arr = [1, 2, 3, 4, 5]
+
+start = 0
+end = len(arr) - 1
+
+while start < end:
+    arr[start], arr[end] = arr[end], arr[start]
+    start += 1
+    end -= 1
+
+print("Reversed array:", arr)
