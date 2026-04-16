fc0266003525ae46e4716240397cab789b40a3eb
Dhaarmi Mewada <mewadadhaarmi@gmail.com>
2026-04-17 00:15:47 +0530

    Add files via upload

---
+++ Union of 2 sorted array.py
@@ -0,0 +1,38 @@
+# Union of two sorted arrays
+
+arr1 = [1, 2, 2, 3, 4]
+arr2 = [2, 3, 5]
+
+i = 0
+j = 0
+union = []
+
+while i < len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+        if not union or union[-1] != arr1[i]:
+            union.append(arr1[i])
+        i += 1
+
+    elif arr1[i] > arr2[j]:
+        if not union or union[-1] != arr2[j]:
+            union.append(arr2[j])
+        j += 1
+
+    else:
+        if not union or union[-1] != arr1[i]:
+            union.append(arr1[i])
+        i += 1
+        j += 1
+
+# Add remaining elements
+while i < len(arr1):
+    if not union or union[-1] != arr1[i]:
+        union.append(arr1[i])
+    i += 1
+
+while j < len(arr2):
+    if not union or union[-1] != arr2[j]:
+        union.append(arr2[j])
+    j += 1
+
+print("Union:", union)
